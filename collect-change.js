bdc9e31517071f4bde9bba34b174d1b22c8b60c8
maserati12 <triggamaserati@gmail.com>
2020-02-07 18:27:24 +0300

    functioning

--- collect-change.js
+++ collect-change.js
@@ -3,6 +3,9 @@ let amountDueDom = document.querySelector('.amount-due');
 let amountPaidDom = document.querySelector('.amount-paid');
 let changeAmountDom = document.querySelector('.change-amount');
 let listWrapper = document.querySelector('.list-wrapper');
+let changeBreakdown = document.querySelector('.change-breakdown');
+let notesWrapper = document.querySelector('.notes-wrapper');
+let coinsWrapper = document.querySelector('.coins-wrapper');
 // Prompts
 let amountDue = prompt('Amount due:');
 let amountPaid = prompt('Amount being paid:');
@@ -10,9 +13,33 @@ let amountPaid = prompt('Amount being paid:');
 amountDue = parseInt(amountDue)
 amountPaid = parseInt(amountPaid)
 let changeAmount;
-amountPaid >= amountDue ? changeAmount = amountPaid - amountDue: changeAmount = amountDue - amountPaid;
+amountPaid >= amountDue ? changeAmount = amountPaid - amountDue: changeAmount = amountPaid - amountDue;
+changeAmount <= 0 ? alert("Please make sure Amount paid is not less than Amount due"): 0;
 // DOM Elements manipulation
 amountDueDom.innerText = "Amount Due: KSh. " + amountDue;
 amountPaidDom.innerText = "Amount Paid: KSh. " + amountPaid;
 changeAmountDom.innerText = "Change: KSh. " + changeAmount;
 listWrapper.style.display = 'block';
+
+let notesArray = [1000, 500, 200, 100, 50];
+let coinsArray = [20, 10, 5, 1];
+notesArray.forEach(note => {
+	if(changeAmount > 0){
+		let notes = Math.floor(changeAmount/note);
+		changeAmount = changeAmount%note;
+		let noteItem = document.createElement('li');
+		noteItem.setAttribute('class',"list-item");
+		notes > 0 ? noteItem.innerHTML = "Ksh " + note + " notes: " + notes: 0;	
+		notesWrapper.appendChild(noteItem)
+		if(changeAmount < 50){
+			coinsArray.forEach(coin => {
+				let coins = Math.floor(changeAmount/coin);
+				changeAmount = changeAmount%coin;
+				let coinItem = document.createElement('li');
+				coinItem.setAttribute('class',"list-item");
+				coins > 0 ? coinItem.innerHTML = "Ksh " + coin + " coins: " + coins: 0;
+				coinsWrapper.appendChild(coinItem)
+			});
+		}
+	}
+})
